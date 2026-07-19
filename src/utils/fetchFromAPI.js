05edd75434772e4637e06342a7f90d396072146c
Bablu Alam <bablu030397@gmail.com>
2026-07-19 20:49:17 +0530

    Add Nginx SPA config and fix API host

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 
-// Update this line in src/utils/fetchFromAPI.js (or wherever it lives)
 export const BASE_URL = 'https://youtube-v311.p.rapidapi.com';
 
 const options = {
-  params: {
-    maxResults: '50',
-  },
+  params: { maxResults: '50' },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com', // Match the URL!
+    // TEMPORARY: Hardcode the key here to see if the UI works.
+    // If this works, the Jenkins injection was the only problem.
+    'X-RapidAPI-Key': '6760343ab2mshdde69ef9c066d1ep14aeb8jsna0db4a81d0dd', 
+    'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com',
   },
 };
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
   return data;
 };
